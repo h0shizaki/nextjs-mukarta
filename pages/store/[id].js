bdb817fea6d82ac66af8350de863a2385382308b
Kitsune-fx <kant.cmd@gmail.com>
2022-03-16 21:35:47 +0700

    [💍 fetch record from api]

--- pages/store/[id].js
+++ pages/store/[id].js
@@ -4,10 +4,13 @@ import Link from "next/link";
 import fetchShabuStores from '../../lib/shabuStore';
 import Head from 'next/head';
 import cls from "classnames";
+import useSWR from 'swr';
 import { useRouter } from 'next/router';
 import { useContext, useState, useEffect } from 'react';
 import { ACTION_TYPES, StoreContext } from '../../store/store-context';
 import { isEmpty } from '../../utils';
+
+export const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export async function getStaticProps(staticProps) {
 
@@ -58,16 +61,42 @@ const Store = (initialProps) => {
     //get init store from prop if not set it empty , then get from context
     const [store, setStore] = useState(initialProps.store || {})
 
-
-    console.log("Init Prop", initialProps.store)
-
     const router = useRouter();
     const id = router.query.id;
+    // console.log(id)
 
+    const handleCreateStoreRecord = async (store) => {
+        try {
+            const { id, name, imgUrl, neighborhood, address } = store;
+            const response = await fetch("/api/createStoreRecord",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        id,
+                        name,
+                        vote: 0,
+                        imgUrl,
+                        neighborhood: neighborhood || "",
+                        address: address || "",
+                    })
+                }
+            );
 
+            const dbStoreRecord = await response.json();
+            // console.log(dbStoreRecord);
+
+        } catch (err) {
+            console.error("Error while creating store", err);
+        }
+    }
+
+    // console.log(isEmpty(initialProps.store));
     useEffect(() => {
-        
 
+        // console.log(isEmpty(initialProps.store));
         if (isEmpty(initialProps.store)) {
 
             if (stores.length > 0) {
@@ -78,14 +107,37 @@ const Store = (initialProps) => {
 
                 if (storeFromContext) {
                     setStore(storeFromContext);
-                    console.log("Context props", storeFromContext)
+                    handleCreateStoreRecord(storeFromContext);
+                    // console.log("Context props", storeFromContext)
                 }
             }
 
         } else {
-
+            // console.log("Init Prop", initialProps.store)
+            handleCreateStoreRecord(initialProps.store)
         }
+
     }, [id, initialProps, initialProps.store, stores])
+
+
+
+    const {
+        address = "",
+        name = "",
+        neighborhood = "",
+        imgUrl = "https://images.unsplash.com/photo-1566705474094-5bf047ce2754?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDA4NzN8MHwxfHNlYXJjaHwzfHxrb3JlYW4lMjBiYnF8ZW58MHx8fHwxNjQ3MzIzNDI1&ixlib=rb-1.2.1&q=80&w=400",
+    } = store;
+
+    const { data, error } = useSWR(`/api/getStoreRecordById?id=${id}`, fetcher)
+
+    useEffect(() => {
+        if (data && data.length > 0) {
+            setStore(data[0]);
+
+            setVotingCount(data[0].vote);
+        }
+    }, [data]);
+
 
     if (router.isFallback) {
         return <div>Loading...</div>;
@@ -95,13 +147,9 @@ const Store = (initialProps) => {
         setVotingCount(votingCount + 1)
     }
 
-    const {
-        address = "",
-        name = "",
-        neighborhood = "",
-        imgUrl = "https://images.unsplash.com/photo-1566705474094-5bf047ce2754?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMDA4NzN8MHwxfHNlYXJjaHwzfHxrb3JlYW4lMjBiYnF8ZW58MHx8fHwxNjQ3MzIzNDI1&ixlib=rb-1.2.1&q=80&w=400",
-    } = store;
-
+    if (error) {
+        return (<div>Something went wrong</div>);
+    }
 
     // const { name, imgUrl, address, neighborhood } = store;
     return (
